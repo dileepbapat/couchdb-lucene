eca0ebbff40595419dd5d42660e6315a69b3f12d
Dileep Bapat <dileepbapat@ubuntu.(none)>
2013-01-15 03:27:31 -0800

    added deb for temporary hosting (till we publish to repo)

--- src/main/tools/debian-build.sh
+++ src/main/tools/debian-build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-cat >src/main/tools/couchdb-lucene-control <<EOF
+cat >target/couchdb-lucene-control <<EOF
 Package: couchdb-lucene
 Version: 0.9.0
 Maintainer: motech <motech-dev@googlegroups.com>
@@ -9,8 +9,8 @@ EOF
 
 cd target/couchdb-lucene-0.9.0-dist;
 mv couchdb-lucene-0.9.0/couchdb-lucene.deb ./couchdb-lucene;
-for f in `find couchdb-lucene-0.9.0/ -type f`; { ff=`dirname $f`;echo " ${f} /usr/share/"; }>>../../src/main/tools/couchdb-lucene-control
+for f in `find couchdb-lucene-0.9.0/ -type f`; { ff=`dirname $f`;echo " ${f} /usr/share/"; }>>../couchdb-lucene-control
 
 
-equivs-build ../../src/main/tools/couchdb-lucene-control
+equivs-build ../couchdb-lucene-control
 cd ../..
